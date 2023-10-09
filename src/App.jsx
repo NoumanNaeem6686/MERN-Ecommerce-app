db3dce00137fdfe3349ca6f0c99a16c4db3940d7
NoumanNaeem6686 <03035777248a@gmail.com>
2023-10-09 13:58:44 +0500

    create header components and links

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import About from './pages/About'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/sign-in' element={<Signin/>}/>
@@ -16,6 +18,7 @@ const App = () => {
       <Route path='/profile' element={<Profile/>}/>
       <Route path='/about' element={<About/>}/>
     </Routes>
+   
      </BrowserRouter>
   )
 }
